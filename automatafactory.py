fa17862b32452afd2fb78bffb17cf2c32dddfeb9
Bill Tosounidis <vasilis.tos@gmail.com>
2017-11-08 13:06:07 +0200

    Finalizing project. Now partially handling exit codes.

--- automatafactory.py
+++ automatafactory.py
@@ -1,9 +1,11 @@
 import argparse
 from getch import _Getch
 from menu import *
+import sys
 
 
 def generate_automata(_infile):
+
     with open(infile, 'r') as fin:
 
         alphabet = []
@@ -67,9 +69,6 @@ class Automata(object):
             for ns in next_states:
                 self.current_states.append(self.states[ns - 1])
 
-        for s in self.current_states:
-            print(s)
-
     def set_alphabet(self, alphabet):
         self.alphabet = alphabet
 
@@ -104,7 +103,7 @@ class State(object):
         return 'Id:{} - Connections:{}'.format(self.id, self.connections)
 
 
-def char_by_char():
+def char_by_char(automata):
 
     getch = _Getch()
 
@@ -118,9 +117,12 @@ def char_by_char():
         x = bytes.decode(getch())
 
         if x == '\r':
-            print('Enter')
-        elif x == '\x1b':
-            sys.exit()
+            if any((int(s.id) in automata.ending_states) for s in automata.current_states):
+                print('\n You are in an end state!\n Continue with another string or press Ctr-C to exit.')
+            else:
+                print('\n You are not in an end state!\n Continue with another string or press Ctr-C to exit.')
+            automata.self_reset()
+            continue
 
         if x not in automata.alphabet:
             print('\r{}'.format(x))
@@ -129,10 +131,9 @@ def char_by_char():
             continue
 
         automata.change_state(x)
-        print(' Current state: {}'.format(automata.current_state.id))
 
 
-def input_string():
+def input_string(automata):
 
     print('\n In this option you can input a string of characters (or even a single character)\n'
           ' and press the return key to see if you are in an end state or not.\n')
@@ -148,14 +149,18 @@ def input_string():
                     automata.self_reset()
                     continue
                 automata.change_state(ch)
-                print(' Current state: {}'.format(automata.current_state.id))
         else:
             if _input not in automata.alphabet:
                 print(' Character {} not in current automata alphabet, please try again.'.format(_input))
                 automata.self_reset()
                 continue
             automata.change_state(_input)
-            print(' Current state: {}'.format(automata.current_state.id))
+
+        if any((int(s.id) in automata.ending_states) for s in automata.current_states):
+            print('\n You are in an end state!\n Continue with another string or press Ctr-C to exit.')
+        else:
+            print('\n You are not in an end state!\n Continue with another string or press Ctr-C to exit.')
+        automata.self_reset()
 
 
 def start_menu():
@@ -176,30 +181,19 @@ if __name__ == '__main__':
 
     automata = generate_automata(infile)
 
-    # print(automata)
-    print(0)
-    automata.change_state(0)
-    print(1)
-    automata.change_state(1)
-    print(0)
-    automata.change_state(0)
-    print(0)
-    automata.change_state(0)
-    print(0)
-    automata.change_state(0)
-    print(1)
-    automata.change_state(1)
-    print(1)
-    automata.change_state(1)
+    try:
+        start_menu()
+        print('\n Current automata alphabet is : {}'.format(automata.alphabet))
 
-    # print(automata)
-    # start_menu()
-    #
-    # while True:
-    #     choice = input('\n [Selection]> ')
-    #     try:
-    #         options[choice]()
-    #     except KeyError:
-    #         print(' Wrong selection! Use one of the numbers provided.')
-    #         continue
-
+        while True:
+            choice = input('\n [Selection]> ')
+            try:
+                if choice == '99':
+                    options[choice]()
+                else:
+                    options[choice](automata)
+            except KeyError:
+                print(' Wrong selection! Use one of the numbers provided.')
+                continue
+    except KeyboardInterrupt:
+        sys.exit()
